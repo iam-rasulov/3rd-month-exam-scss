1f0f5cff714568901f0aed1dfc1ef3ded9b84f7a
iam-rasulov <mirjalolrasulov002@gmail.com>
2022-03-06 21:20:09 +0500

    7th commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var elBtn = document.querySelector(".right__btn")
+var elHero = document.querySelector(".hero")
+var elImg = document.querySelector(".inner__img")
+var elList = document.querySelector(".right__list2")
+
+elBtn.addEventListener("click" , function (){
+    elHero.classList.toggle("d-none")
+    elImg.classList.toggle("d-none")
+    elList.classList.toggle("d-d-block")
+
+})
+
+
